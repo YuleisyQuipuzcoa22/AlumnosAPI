a8bb2332f4522dec702f364f04a05d51fe3649d7
Belen <anabelensanchezboy@gmail.com>
2025-10-06 19:50:39 -0500

    Entidad

---
+++ alumnos-api/src/main/java/example/com/entity/Alumno.java
@@ -0,0 +1,74 @@
+package example.com.entity;
+
+import java.time.LocalDateTime;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "alumno")
+public class Alumno {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String nombres;
+    private String apellidos;
+    private String codigo;
+    private String grupo;
+
+    //Constructor vacío
+    public Alumno() {
+    }
+
+    public Alumno(String nombres, String apellidos, String codigo, String grupo) {
+        this.nombres = nombres;
+        this.apellidos = apellidos;
+        this.codigo = codigo;
+        this.grupo = grupo;
+    }
+
+    // Gets y setters
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNombres() {
+        return nombres;
+    }
+
+    public void setNombres(String nombres) {
+        this.nombres = nombres;
+    }
+
+    public String getApellidos() {
+        return apellidos;
+    }
+
+    public void setApellidos(String apellidos) {
+        this.apellidos = apellidos;
+    }
+
+    public String getCodigoEstudiante() {
+        return codigo;
+    }
+
+    public void setCodigoEstudiante(String codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getGrupo() {
+        return grupo;
+    }
+
+    public void setGrupo(String grupo) {
+        this.grupo = grupo;
+    }
+
+}
