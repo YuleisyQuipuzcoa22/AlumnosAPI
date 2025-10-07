6ac14d7a74da2d3ecef65dac730e36b89ebc9a70
Belen <anabelensanchezboy@gmail.com>
2025-10-06 21:22:23 -0500

    conexion BD

--- alumnos-api/src/main/java/example/com/repository/AlumnoRepository.java
+++ alumnos-api/src/main/java/example/com/repository/AlumnoRepository.java
@@ -1,10 +1,23 @@
 package example.com.repository;
 
+import java.util.List;
+
 import org.springframework.stereotype.Repository;
+
+import example.com.entity.Alumno;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
 
 // Acceso a Datos
 // Se comunica directamente con la BD 
 @Repository
 public class AlumnoRepository {
-    
+    @PersistenceContext
+    private EntityManager entityManager;
+
+    // Método para obtener todos los alumnos
+    public List<Alumno> findAll() {
+        return entityManager.createQuery("SELECT a FROM Alumno a", Alumno.class)
+                .getResultList();
+    }
 }
